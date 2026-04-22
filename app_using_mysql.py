38e0f0e0bfab7fd52917112981160aaba7f1e429
jd878 <jd.878@njit.edu>
2026-04-22 18:38:24 -0400

    Updated README, removed generated files

--- app_using_mysql.py
+++ app_using_mysql.py
@@ -2,14 +2,17 @@ import mysql.connector
 from mysql.connector import Error
 
 from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 def create_connection():
     try:
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="jeet1108",
-            database="library_db"
+        host = os.getenv('DB_HOST'),
+        user = os.getenv('DB_USER'),
+        password = os.getenv('DB_PASSWORD'),
+        database = os.getenv('DB_NAME')
         )
         return connection
     except Error as e:
